b25045f55968a8d5677cffbebf4d81f956587f01
Khoirul Asfian <mk.asfian@gmail.com>
2022-09-27 23:24:44 +0700

    fix: add db_port & db_options

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,11 +1,20 @@
 import { Client } from "postgres/mod.ts";
-import { DB_HOST, DB_NAME, DB_PASSWORD, DB_USER } from "./environment.ts";
+import {
+  DB_HOST,
+  DB_NAME,
+  DB_OPTIONS,
+  DB_PASSWORD,
+  DB_PORT,
+  DB_USER,
+} from "./environment.ts";
 
 const db = new Client({
   hostname: DB_HOST ?? "127.0.0.1",
   user: DB_USER ?? "postgres",
   password: DB_PASSWORD ?? "",
   database: DB_NAME ?? "",
+  port: DB_PORT ?? 26257,
+  options: DB_OPTIONS ?? "",
 });
 
 export default db;
--- src/utils/environment.ts
+++ src/utils/environment.ts
@@ -3,4 +3,6 @@ export const {
   DB_PASSWORD = "11223344",
   DB_USER = "postgres",
   DB_NAME = "tokoline",
+  DB_PORT = 26257,
+  DB_OPTIONS = "",
 } = Deno.env.toObject();
